7d71e6320c5d51ad188d019d3a6a2a3dd9fac3b4
Vessela Marinova <vessela.v.marinova@gmail.com>
2024-09-22 00:30:22 +0300

    registration form, no link

--- src/app/shared/components/login-form/login-form.component.ts
+++ src/app/shared/components/login-form/login-form.component.ts
@@ -19,6 +19,8 @@ export class LoginFormComponent {
 
   submitLoginHandler(loginForm:NgForm) {
     this.formSubmitted =  true;
+    console.log(loginForm);
+    
   }
 }
 
--- src/app/shared/components/registration-form/registration-form.component.ts
+++ src/app/shared/components/registration-form/registration-form.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { EmailValidatorDirective } from '@app/shared/directives/email.directive';
 
 @Component({
   selector: 'app-registration-form',
   templateUrl: './registration-form.component.html',
   styleUrls: ['./registration-form.component.scss'],
 })
-export class RegistrationFormComponent {
+export class RegistrationFormComponent implements OnInit {
   registrationForm!: FormGroup;
   // Use the names `name`, `email`, `password` for the form controls.
+ 
+ 
+ 
+  formSubmitted:boolean=false;
+
+ ngOnInit(): void {
+   this.registrationForm= new FormGroup({
+    'name':new FormControl(null, [Validators.required, Validators.minLength(6)]),
+    'email':new FormControl(null,[Validators.required]),
+    'password':new FormControl(null, Validators.required),
+   });
+ }
+
+
+ submitRegistrationHandler (registrationForm:FormGroup) {
+  this.formSubmitted=true;
+  console.log(this.registrationForm);
+  
+ }
+
+ 
+ 
+  registrationBtnText:string="login"
 }
+  
