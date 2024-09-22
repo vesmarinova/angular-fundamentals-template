347b4da3be43244b01f2df6f4231011698c73a6e
Vessela Marinova <vessela.v.marinova@gmail.com>
2024-09-22 07:54:49 +0300

    @Input() placeholder!

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -9,7 +9,7 @@ export class SearchComponent {
   // Use the name `placeholder` for the @Input.
   // Use the name `search` for the @Output.
 
-  @Input() placeholder:string='';
+  @Input() placeholder!:string;
   @Output() search = new EventEmitter<string>();
 
   searchQuery(value: string) {
